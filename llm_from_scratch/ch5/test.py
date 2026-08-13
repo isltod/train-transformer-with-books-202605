8aa465d39c6daa9e884f5225ffe6b26855feab3b
E. K. Yi <wolf@teoal.net>
2026-08-13 15:23:26 +0900

    초기 lr warmup 실습

--- llm_from_scratch/ch5/test.py
+++
@@ -1,8 +0,0 @@
-import os
-from pathlib import Path
-import sys
-
-data_dir = os.path.join("e:\\Devs\\train-transformer-with-books-202605\\data\\")
-print(os.listdir(data_dir))
-save_dir = Path(__file__).resolve().parents[2] / "data"
-print(os.listdir(save_dir))
